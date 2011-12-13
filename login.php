4fa7f295e53dc991bf1ec1f448637c1f18e23827
Michael Brogan <projects@netboffin.com>
2011-12-13 16:15:18 +0000

    got the login working

--- login.php
+++ login.php
@@ -9,13 +9,24 @@
 	mysql_select_db("netboffi_goject");
 	$query = "select * from account where AccountUsername = '$username' and AccountPasswrd = '$hash'";
 	$result = mysql_query($query)or die("query didn't work".mysql_error());
+	
 	if(mysql_num_rows($result) == 0)
 	{
 		print "No user with this username: $username and password $passwrd";
 	}
-	print $hash;
+	
 	while($row = mysql_fetch_array($result))
 	{
+		if($row["AccountVerified"])
+		{
+			print "<p>".$row["AccountVerified"]."</p>";
+			print "This account is verified, you are now logged in";
+		} else 
+		{
+			print "<p>".$row['AccountVerified']."</p>";
+			print "This account is not verified so you need to find the email we sent you and click the link";
+		}
 		print_r($row);
-	}		
+	}
+			
 ?>
