fbbd0ef07ac2d89e6b0de5537a2f2685b1882d95
Michael Brogan <projects@netboffin.com>
2011-12-18 19:11:19 +0000

    started adding security question to password reset

--- password_reminder.php
+++ password_reminder.php
@@ -10,6 +10,9 @@ if(isset($_POST["stage"]) && ($_POST["stage"]=='process'))
 {
 	display_form();
 }
+
+
+
 function display_form(){
 ?>
 <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
@@ -21,24 +24,46 @@ function display_form(){
 }
 function process_password_reset_form(){
 	$usernameoremail = $_POST["usernameoremail"];
-	//connect to database
+	//does the username or email exist?
 	mysql_connect("localhost","netboffi","l2315793014");
 	mysql_select_db("netboffi_goject");
+	
 	$query = "select * from account where AccountUsername = '$usernameoremail' or AccountEmail = '$usernameoremail'";
 	$result = mysql_query($query);
-	var_dump($result);
-	if(($rows = mysql_fetch_array($result))>0)
+	
+	
+	//if they haven't set a security question just send them an email with the new password
+	// if they have set a security question then display a form to get the answer for the security question. 
+	if(($rows = mysql_fetch_object($result))>0)
 	{
 		print_r($rows);
-		$emailAddress = $rows["AccountEmail"];
-		$new_password = uniqid(rand(),TRUE);
-		$salt = "Mick";
-		$passwordandhash = $new_password.$salt;
-		$hash = md5($passwordandhash);
-		$query2 = "update account set AccountPasswrd = '$hash'";		
-		mysql_query($query2)or die("<p>Couldn't update account password ".mysql_error());
-		mail($emailAddress,"Password Reset Here's your new password",$new_password);
-		print "Your new password has been sent to".$emailAddress;
+		if($rows->AccountSecurityQuestionSet == true)
+		{
+			// display security question form
+			?>
+			<form id="security_question_form" action = "<?php echo $_SERVER['PHP_SELF']?>" method = "post">
+			<p><label><?=$rows->AccountSecurityQuestion?></label></p>
+			<p><input type="text" name="security_question" width="50"/></p>	
+			<input type="hidden" name="security_question" value="process"/>		
+			</form>
+			<?php 
+			
+		}else 
+		{
+			// otherwise just send 
+			$emailAddress = $rows->AccountEmail;
+			$new_password = uniqid(rand(),TRUE);
+			$salt = "Mick";
+			$passwordandhash = $new_password.$salt;
+			$hash = md5($passwordandhash);
+			$query2 = "update account set AccountPasswrd = '$hash'";		
+			mysql_query($query2)or die("<p>Couldn't update account password ".mysql_error());
+			mail($emailAddress,"Password Reset Here's your new password",$new_password);
+			print "Your new password has been sent to".$emailAddress;
+			
+			
+		}
+		
 	}	
 }
 
